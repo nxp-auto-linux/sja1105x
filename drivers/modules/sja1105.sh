f7f0d924d1b0af55ebc44d733cf1f2bf79963a82
Costin Carabas <costin.carabas@nxp.com>
2017-11-20 17:23:26 +0200

    init script: Add Init Script Header
    This change allows no errors when converting this init script to systemd service
    unit scripts (used in later versions of Ubuntu). This is achieved by
    systemd-sysv-generator, which generates the service units that run the System V
    rc scripts from /etc/init.d, if it doesn't find a native systemd service unit by
    that name in default locations.
    
    Issue: ALB-1889
    Signed-off-by: Costin Carabas <costin.carabas@nxp.com>

--- drivers/modules/sja1105.sh
+++ drivers/modules/sja1105.sh
@@ -1,6 +1,16 @@
 # Copyright 2017 NXP
 
-KERN=4.1.26-rt29
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          sja1105
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     3 5
+# Default-Stop:
+# Short-Description: Loading sja1105 module
+# Description:       Inserts SJA1105PQRS kernel module. This enables ethernet.
+### END INIT INFO
+
 OPT="max_hz=5000 verbosity=1"
-rmmod sja1105.ko
+lsmod | grep sja1105 > /dev/null && rmmod sja1105
 insmod /lib/modules/`uname -r`/extra/sja1105.ko ${OPT}
